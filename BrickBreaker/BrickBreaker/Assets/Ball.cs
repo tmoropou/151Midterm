f5e7253001fbc40c533ee47fe58f91ac26c83107
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2021-02-02 14:31:45 -0800

    Added Vertical Speed Normalizer and Paddle angle correction
    * When the ball is moving and its vertical speed is low (ex, stuck moving from side to side with 0 vertical speed), its vertical speed is increased to break this loop.
    *When the ball hits the paddle, it is launched at an angle corresponding to its hit position, as in classic breakout.

--- BrickBreaker/BrickBreaker/Assets/Ball.cs
+++ BrickBreaker/BrickBreaker/Assets/Ball.cs
@@ -14,14 +14,36 @@ public class Ball : MonoBehaviour
         Respawn();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void Respawn() {
         transform.position = Vector3.zero;
         GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+    }
+
+    void Update() {
+        // normalizes vertical speed if too high or too low
+        if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) < .4) {
+            // Debug.Log("Vertical speed increased from " + GetComponent<Rigidbody2D>().velocity.y.ToString() + " to " + (GetComponent<Rigidbody2D>().velocity.y * 2).ToString());
+            if(GetComponent<Rigidbody2D>().velocity.y == 0) {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, -0.4f);
+            }
+            else {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 2);
+            }
+        }
+        if(Mathf.Sqrt(Mathf.Pow(GetComponent<Rigidbody2D>().velocity.y, 2) * Mathf.Pow(GetComponent<Rigidbody2D>().velocity.x, 2)) > 9) {
+            // Debug.Log("Vertical Speed over 9, reducing.");
+            GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity * .9f;
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Paddle") {
+            // use "(position - position) / width" for proportional to size
+            float distance = (transform.position.x - other.gameObject.transform.position.x) / 1.0f;
+            //Debug.Log(1 - Mathf.Abs(distance));
+            GetComponent<Rigidbody2D>().velocity = new Vector2(1.1f * distance, 1 - Mathf.Abs(distance)) * speed;
+            //Debug.Log(transform.position.x - other.gameObject.transform.position.x ); // need to make reflection based on position
+        }
     }
 }
